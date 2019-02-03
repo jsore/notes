6482d9797581f268d9109f65e5f7e062cb401199
jsorensen <sorensej@us.ibm.com>
2019-02-03 14:19:45 -0600

    Refined notes for better hand-holding through the code

--- mastering-node-js/project-files/http/http-tunneling.js
+++ mastering-node-js/project-files/http/http-tunneling.js
@@ -6,23 +6,26 @@ const http = require('http');
 const net = require('net');
 const url = require('url');
 
-/*----------  the tunnel on 8080  ----------*/
+/*----------  1st: the tunnel on 8080  ----------*/
 
 /** set up a proxy server listening for HTTP CONNECT requests */
 const proxy = new http.Server();
+
+/*----------  4th: the proxy'ed request via tunnel  ----------*/
+
 /** set up a bridge with client socket and remote socket */
 proxy.on('connect', (request, clientSocket, head) => {
-
-    /*----------  the proxy'ed request via tunnel  ----------*/
-
-    /** use the 'path' portion of the client request */
+    /** use the 'path' portion of the 'client' request */
     let reqData = url.parse(`http://${request.url}`);
     /** take 'client' request, connect to the remote */
     let remoteSocket = net.connect(reqData.port, reqData.hostname, () => {
-        /** send over a 200 OK to the client */
+        /** send over a 200 OK header to the client */
         clientSocket.write('HTTP/1.1 200 \r\n\r\n');
-        /** send the client's header to the remote */
+        /** send the client's request header to the remote */
         remoteSocket.write(head);
+
+        /** begin working with request body */
+
         /**
          * send the remote a copy of the client's request,
          * which should recieve a response that was written
@@ -39,8 +42,9 @@ proxy.on('connect', (request, clientSocket, head) => {
  * request to it, mimicking a client
  */
 
-/*----------  the 'client' connecting and requesting to tunnel  ----------*/
+/*----------  2nd: the 'client' connecting and requesting to tunnel  ----------*/
 
+/** returns http.ClientRequest instance (a Writable stream) */
 let request = http.request({
     port: 8080,
     hostname: 'localhost',
@@ -53,22 +57,24 @@ let request = http.request({
  */
 request.end();
 
-/*----------  the 'client' tunneled GET after connecting  ----------*/
+/*----------  3rd: the 'client' tunneled GET after connecting  ----------*/
 
 request.on('connect', (res, socket, head) => {
     socket.setEncoding("utf8");
-    /** send a _write to GET a response */
+    /** send a header via _write to GET a response */
     socket.write('GET / HTTP/1.1\r\nHost: www.example.org:80\r\nConnection: close\r\n\r\n');
     /** on response (a 'read' event being emitted)... */
     socket.on('readable', () => {
         /**
          * ...oblige the read event, which will fire an
-         * end() event by the read stream sending 'null'
+         * end() event via the read stream sending 'null'
          * after it has nothing left to read
          */
         console.log(socket.read());
     });
-    /** shut down the proxy */
+
+    /*----------  5th, final: shut down the proxy  ----------*/
+
     socket.on('end', () => {
         proxy.close();
     });
